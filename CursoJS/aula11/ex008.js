7ee2280137438419d1291ec8e9a914eef49c2f52
victorramos887 <v.ramos58@hotmail.com>
2022-09-10 20:08:06 -0300

    aula de condicionais

---
+++ CursoJS/aula11/ex008.js
@@ -0,0 +1,11 @@
+console.log('Escrevendo na tela')
+
+var velocidade = 75;
+
+if(velocidade > 60) {
+
+    console.log('Você utrapassou o limite de velocidade!!!')
+
+}
+
+console.log('Dirija sempre com segurança!!!')
