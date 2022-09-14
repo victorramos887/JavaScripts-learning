0bc02d24431a3dfd9e8863fd3d9da416d4eb87af
victorramos887 <v.ramos58@hotmail.com>
2022-09-14 19:33:47 -0300

    ajustes de exercicios

--- CursoJS/exercicio01/exercicio15/scripts/script.js
+++ CursoJS/exercicio01/exercicio15/scripts/script.js
@@ -1,7 +1,14 @@
 function funcao() {
 
-    var data = window.document.getElementById("date__nascimento").value
+    var data = new Date(window.document.getElementById("date__nascimento").value)
     var debug = window.document.getElementById("debug")
+    var dias = (1000*60*60*24);
+    data_atual = new Date()
+    var diasdevida = (data_atual.getTime() - data.getTime())/dias
 
     debug.innerHTML = data
+    debug.innerHTML += '<br>'
+    debug.innerHTML += data_atual + '<br>'
+    
+    debug.innerHTML += diasdevida
 }
