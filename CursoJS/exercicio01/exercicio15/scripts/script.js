4310ec2361e620d86210e3dae1850d366c7a0d29
victorramos887 <v.ramos58@hotmail.com>
2022-09-14 13:39:58 -0300

    criando verificador

---
+++ CursoJS/exercicio01/exercicio15/scripts/script.js
@@ -0,0 +1,7 @@
+function funcao() {
+
+    var data = window.document.getElementById("date__nascimento").value
+    var debug = window.document.getElementById("debug")
+
+    debug.innerHTML = data
+}
