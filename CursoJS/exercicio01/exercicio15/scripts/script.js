ac6553a6945a2bb729fa1ee664f6cf7197ff1344
victorramos887 <v.ramos58@hotmail.com>
2022-09-16 10:03:45 -0300

    EXERCICIOS
    VERIFICADOR DE IDADE

--- CursoJS/exercicio01/exercicio15/scripts/script.js
+++ CursoJS/exercicio01/exercicio15/scripts/script.js
@@ -1,14 +1,63 @@
-function funcao() {
+function verificar() {
 
-    var data = new Date(window.document.getElementById("date__nascimento").value)
-    var debug = window.document.getElementById("debug")
-    var dias = (1000*60*60*24);
-    data_atual = new Date()
-    var diasdevida = (data_atual.getTime() - data.getTime())/dias
+    var data = new Date()
+    var ano = data.getFullYear()
+    var resultado = window.document.getElementById("resultado")
+    var anoDigitado = window.document.getElementById("txtyear")
 
-    debug.innerHTML = data
-    debug.innerHTML += '<br>'
-    debug.innerHTML += data_atual + '<br>'
+    if(anoDigitado.value.length == 0 || Number(anoDigitado) > ano) {
+        window.alert("[ERRO] Verifique os dados e tente novamente")
+    } else{
+        var fsex = window.document.getElementsByName("radsex")
+        var idade = ano - Number(anoDigitado.value)
+        var genero = ''
+        var img = document.createElement('img')
+
+        img.setAttribute('id', 'picture')
+
+        if(fsex[0].checked) {
+            
+            genero='Homem'
+
     
-    debug.innerHTML += diasdevida
+
+            if(idade >=0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', `./imagens/h_crianca.png`)
+            } else if(idade <21) {
+                //Jovem
+                img.setAttribute('src', './imagens/h_jovem.png')
+            } else if(idade < 45){
+                //adulto
+                img.setAttribute('src', './imagens/h_adulto.png')
+            } else {
+                //velho
+                img.setAttribute('src', './imagens/h_velho.png')
+            }
+
+        } else{
+            genero='Mulher'
+
+            if(idade >=0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', './imagens/m_crianca.png')
+            } else if(idade <21) {
+                //Jovem
+                img.setAttribute('src', './imagens/m_jovem.png')
+            } else if(idade < 45){
+                //adulto
+                img.setAttribute('src', './imagens/m_adulta.png')
+            } else {
+                //velho
+                img.setAttribute('src', './imagens/m_velha.png')
+            }
+        }
+
+
+        resultado.style.textAlign = 'center'
+        resultado.innerHTML = `idade = ${idade}` 
+        resultado.innerHTML += genero
+        resultado.appendChild(img)
+    }
+
 }
