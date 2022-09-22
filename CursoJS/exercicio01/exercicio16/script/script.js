2dda11a137ae0ea171bce6835c043b58aa8bd545
victorramos887 <v.ramos58@hotmail.com>
2022-09-22 11:40:06 -0300

    exercicio 16

--- CursoJS/exercicio01/exercicio16/script/script.js
+++ CursoJS/exercicio01/exercicio16/script/script.js
@@ -7,13 +7,23 @@ function run() {
     var resultado = window.document.getElementById('resultado')
     var contador = inicio
     resultado.innerHTML = ''
-
-    while( contador < Number(fim) ) {
+    var check = 0;
+    while( check == 0 ) {
 
         if(step != 0) {
 
             if(inicio > fim){
-                
+                resultado.innerHTML += (((contador - step) <= fim) ? `&#128073;&#127998; ${contador} &#127937; `:`&#128073;&#127998; ${contador} /`)
+                contador -= step
+                if (contador == fim) {
+                    check = 1;
+                }
+            } else {
+                resultado.innerHTML += (((contador + step) >= fim) ? `&#128073;&#127998; ${contador} &#127937;`:`&#128073;&#127998; ${contador} /`)
+                contador += step
+                if(contador == fim) {
+                    check =1;
+                }
             }
 
             
