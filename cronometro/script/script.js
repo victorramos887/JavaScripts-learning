575a5341ad8d23176bfaf4a0969d08ec378b3843
victorramos887 <v.ramos58@hotmail.com>
2022-09-22 15:34:22 -0300

    FINALIZANDO CRONOMETRO

--- cronometro/script/script.js
+++ cronometro/script/script.js
@@ -1,69 +1,65 @@
 
-// let hour = 0;
-// let minute = 0;
-// let second = 0;
-// let millisecond = 0;
+let hour = 0;
+let minute = 0;
+let second = 0;
+let millisecond = 0;
 
-// let cron;
+let cron;
 
 // document.form_main.start.onclick = () => start();
 // document.form_main.pause.onclick = () => pause();
 // document.form_main.reset.onclick = () => reset();
 
-//var buttonStart = window.document.getElementById('iniciar')
-// var buttonPause = window.document.getElementById('pause')
-// var buttonReset = window.document.getElementById('resetar')
+var buttonStart = window.document.getElementById('iniciar')
+var buttonPause = window.document.getElementById('pause')
+var buttonReset = window.document.getElementById('resetar')
 
-//buttonStart.addEventListener('click', debug)
-// buttonPause.addEventListener('click', pause)
-// buttonReset.addEventListener('click', reset)
+buttonStart.addEventListener('click', start)
+buttonPause.addEventListener('click', pause)
+buttonReset.addEventListener('click', reset)
 
-function debug(){
-    // var valor = window.document.getElementById('debug')
-
-    // valor.innerHTML += 'ativo'
-    alert('Alerrt')
+function start() {
+  pause();
+  cron = setInterval(() => { timer(); }, 10);
 }
 
-// function start() {
-//   pause();
-//   cron = setInterval(() => { timer(); }, 10);
-// }
+function pause() {
+  clearInterval(cron);
+}
 
-// function pause() {
-//   clearInterval(cron);
-// }
+function reset() {
 
-// function reset() {
-//   hour = 0;
-//   minute = 0;
-//   second = 0;
-//   millisecond = 0;
-//   document.getElementById('time__hora').innerText = '00';
-//   document.getElementById('time__minutos').innerText = '00';
-//   document.getElementById('time__segundos').innerText = '00';
-//   document.getElementById('time__milisegundos').innerText = '000';
-// }
+    
+  hour = 0;
+  minute = 0;
+  second = 0;
+  millisecond = 0;
+  document.getElementById('time__hora').innerText = '00';
+  document.getElementById('time__minutos').innerText = '00';
+  document.getElementById('time__segundos').innerText = '00';
+  document.getElementById('time__milisegundos').innerText = '000';
+  pause();
+}
 
-// function timer() {
-//   if ((millisecond += 10) == 1000) {
-//     millisecond = 0;
-//     second++;
-//   }
-//   if (second == 60) {
-//     second = 0;
-//     minute++;
-//   }
-//   if (minute == 60) {
-//     minute = 0;
-//     hour++;
-//   }
-//   document.getElementById('time__hora').innerText = returnData(hour);
-//   document.getElementById('time__minutos').innerText = returnData(minute);
-//   document.getElementById('time__segundos').innerText = returnData(second);
-//   document.getElementById('time__milisegundos').innerText = returnData(millisecond);
-// }
+function timer() {
+  if ((millisecond += 10) == 1000) {
+    millisecond = 0;
+    second++;
+  }
+  if (second == 60) {
+    second = 0;
+    minute++;
+  }
+  if (minute == 60) {
+    minute = 0;
+    hour++;
+  }
+  document.getElementById('time__hora').innerText = returnData(hour);
+  document.getElementById('time__minutos').innerText = returnData(minute);
+  document.getElementById('time__segundos').innerText = returnData(second);
+  document.getElementById('time__milisegundos').innerText = returnData(millisecond);
+}
 
-// function returnData(input) {
-//   return input >= 10 ? input : `0${input}`
-// }
+function returnData(input) {
+  return input >= 10 ? input : `0${input}`
+}
