39ff7af6df3ba1ea5eaf5932c93f0ab0580a6b6a
victorramos887 <v.ramos58@hotmail.com>
2022-09-22 20:20:50 -0300

    Criando um cronometro
    Criando um cronometro igual ao Xaiomi 7

---
+++ cronometro_xaiomi/script/script.js
@@ -0,0 +1,27 @@
+let minutes = 0;
+let second = 0;
+let milisecond = 0;
+
+let s_milisecond = window.document.getElementById('miliseconds');
+let s_second = window.documento.getElementById('seconds');
+let s_minutes = window.document.getElementById('minutes');
+
+//BUTTON 
+let play = window.document.getElementById('play');
+let cron;
+
+// ATRIBUIR FUNÇÕES
+play.addEventListener("click", start);
+
+
+function start(){
+    alert('Victor')
+};
+
+function pause() {
+
+};
+
+function reset() {
+
+};
