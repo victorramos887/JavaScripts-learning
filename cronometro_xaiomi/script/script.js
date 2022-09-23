0a48ab90545208fe170a0957dbf86dab24503bd3
victorramos887 <v.ramos58@hotmail.com>
2022-09-23 10:33:19 -0300

    cronometro

--- cronometro_xaiomi/script/script.js
+++ cronometro_xaiomi/script/script.js
@@ -2,26 +2,93 @@ let minutes = 0;
 let second = 0;
 let milisecond = 0;
 
-let s_milisecond = window.document.getElementById('miliseconds');
-let s_second = window.documento.getElementById('seconds');
-let s_minutes = window.document.getElementById('minutes');
+var smilisecond = window.document.getElementById('miliseconds');
+var ssecond = window.document.getElementById('seconds');
+var sminutes = window.document.getElementById('minutes');
+
+//SET DIV
+var sideTop = window.document.getElementById('side__top')
 
 //BUTTON 
-let play = window.document.getElementById('play');
-let cron;
+var play = window.document.getElementById('play');
+var cron;
+var controleButton = 1;
 
 // ATRIBUIR FUNÇÕES
 play.addEventListener("click", start);
 
 
 function start(){
-    alert('Victor')
+    
+    if( controleButton == 1 ) {
+
+        sideTop.innerHTML = ''
+        pause()
+        cron = setInterval(() => {timer(); }, 10);
+        controleButton = 0;
+
+        // INSERINDO OUTRO BOTÃO
+        var reset = window.document.createElement("button")
+        reset.setAttribute('class', 'reset')
+        reset.setAttribute('type', 'button')
+
+        reset.addEventListener('click', function reset() {
+
+            minutes = 0;
+            second = 0;
+            milisecond = 0;
+        
+            smilisecond.innerText = '00';
+            ssecond.innerText = '00';
+            sminutes.innerText = '00';
+            pause();
+            sideTop.innerHTML = ''
+          })
+
+        sideTop.appendChild(reset)
+
+    } else {
+
+        pause()
+        controleButton = 1;
+        
+
+    }
 };
 
 function pause() {
+    clearInterval(cron);
+}
 
-};
+function timer() {
+    if((milisecond += 10)==1000) {
 
-function reset() {
+        milisecond = 0;
+        second ++;
+    }
 
-};
+    if(second == 60) {
+        second = 0;
+        minutes ++
+    }
+
+    if(minutes==60) {
+        second = 0;
+        minutes = 0;
+        milisecond = 0;
+    }
+    
+    smilisecond.innerText = returnData(milisecond)
+    ssecond.innerText = returnData(second)
+    sminutes.innerText = returnData(minutes)
+    
+}
+// function pause() {
+
+// };
+
+
+
+function returnData(input) {
+    return input >= 10 ? input:`0${input}`
+}
